b4cc634900a80d8935ce615ea9a9d2d24d2c0ec2
Rifat172 <45rifat7227@gmail.com>
2019-08-01 16:36:35 +0500

    переместил папки

--- CoinApp/Controllers/ContentController.cs
+++ CoinApp/Controllers/ContentController.cs
@@ -2,23 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using CoinApp.Models;
 using System.Web.Mvc;
-using System.Data.Entity;
 
 namespace CoinApp.Controllers
 {
-    public class HomeController : Controller
+    public class ContentController : Controller
     {
-        public ActionResult Main()
+        public ActionResult CryptocurrencyQuotes()
         {
             return View();
         }
-
-        
-
-        
-
-       
     }
 }
